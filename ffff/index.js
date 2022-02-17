244683729e51e071d6e5e2897a0d6e8db42b8fc7
ZaharTrofym <52045407+ZaharTrofym@users.noreply.github.com>
2022-02-17 21:28:25 +0200

    Add files via upload

---
+++ ffff/index.js
@@ -0,0 +1,339 @@
+// 1
+// let num = 3
+// alert(num)
+
+
+// 2
+// let a = 10, b = 2
+// console.log(a + b, a - b, a * b, a / b)
+
+
+// 3
+// let c = 15, d = 2, result = c + d
+// console.log(result)
+
+
+// 4
+// let a = 10, b = 2, c = 5
+// console.log(a + b + c)
+
+
+// 5
+// let a = 17, b = 10, c = a - b, d = 7, result = c + d
+// console.log(result)
+
+
+// 6
+// let str = "Привет, Мир!"
+// console.log(str)
+
+
+// 7
+// let str1 = 'Привет, ', str2='Мир!'
+// console.log(str1 + str2)
+
+
+// 8
+// let name = "Акакий"
+// console.log(`Привет, ${name}!`)
+
+
+// 9
+// let age = 9
+// console.log(`Мне ${age} лет!`)
+
+
+// 10
+// let name = prompt("Введите ваше имя: ")
+// alert(`Ваше имя ${name}`)
+
+
+// 11
+// let num = prompt("Введите число: ")
+// console.log(num*=num)
+
+// 12
+// let str = "abcde"
+// console.log(str[0], str[2], str[4])
+
+
+// 13
+// let num = "12345"
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4])
+
+
+// 14
+// console.log(`В минуте ${60} секунд, в одном часе ${60*60} секунд, в одном дне ${60*60*24} секунд.`)
+
+
+// 15
+// let hour = 16, minute = 33, second = 25
+// console.log(`${hour}:${minute}:${second}`)
+
+
+// 16
+// let num = 23
+// console.log(num ** 2)
+
+
+// 17
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+  
+
+// 18
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+
+
+
+ 
+// 1
+// let a = -3
+// if (a == 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 2
+// let a = -3
+// if (a > 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 3
+// let a = -3
+// if (a < 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 4
+// let a = -3
+// if (a >= 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 5
+// let a = -3
+// if (a <= 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 6
+// let a = -3
+// if (a != 0) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 7
+// let a = "test"
+// if (a == "test") {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 8
+// let a = "1"
+// if (a == 1) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 9
+// let test = false
+// if (test == true) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+// let test = true
+// switch(test) {
+//     case true:
+//         alert("Верно")
+//     break;
+//     case false:
+//         alert("Неверно")
+//     break;
+// }
+
+
+// 10
+// let test = false
+// if (test != true) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+// let test = true
+// switch(test) {
+//     case false:
+//         alert("Верно")
+//     break;
+//     case true:
+//         alert("Неверно")
+//     break;
+// }
+
+
+// 11
+// let a = 2
+// if (a > 0 && a < 5) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Неверно")
+// }
+
+
+// 12 
+// let a = 2
+// if (a == 0 || a == 2) {
+//     a += 7
+// }
+// else {
+//     a /= 10
+// }
+// console.log(a)
+
+
+// 13
+// let a = 3, b = 5
+// if (a <= 1 && b >= 3) {
+//     console.log(a + b)
+// }
+// else {
+//     console.log(a - b)
+// }
+
+
+// 14
+// let a = 3, b = 5
+// if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+//     console.log("Верно")
+// }
+// else {
+//     console.log("Неверно")
+// }
+
+
+// 15
+// let num = 4
+// switch (num) {
+//     case 1:
+//         console.log("зима")
+//     break;
+//     case 2:
+//         console.log("весна")
+//     break;
+//     case 3:
+//         console.log("лето")
+//     break;
+//     case 4:
+//         console.log("осень")
+//     break;
+// }
+
+
+// 16
+// let day = 1
+// if (day > 0 || day < 11) {
+//     console.log(1)
+// }
+// else if (day > 10 || day < 21) {
+//     console.log(2)
+// }
+// else if (day > 20 || day < 32) {
+//     console.log(3)
+// }
+
+
+// 17
+// let month = 1
+// if ((month > 0 && month < 3) || month == 12) {
+//     console.log("зима")
+// }
+// else if (month > 2 && month < 6) {
+//     console.log("лето")
+// }
+// else if (month > 5 && month < 9) {
+//     console.log("весна")
+// }
+// else if (month > 8 && month < 12) {
+//     console.log("осень")
+// }
+
+
+// 18
+// let str = "abcde"
+// if (str[0] == "a") {
+//     
+// }
+// else {
+//     console.log("нет")
+// }
+
+
+// 19
+// let str = "12345"
+// if (str[0] == 1 || str[0] == 2 || str[0] == 3) {
+//     console.log("да")
+// }
+// else {
+//     console.log("нет")
+// }
+
+
+// 20
+// let str = "123"
+// console.log(Number(str[0]) + Number(str[1]) + Number(str[2]))  
+
+
+// 21
+// let str = "123123"
+// if (Number(str[0]) + Number(str[1]) + Number(str[2]) == Number(str[3]) + Number(str[4]) + Number(str[5])) {
+//     console.log("да")
+// }
+// else {
+//     console.log("нет")
+// }
